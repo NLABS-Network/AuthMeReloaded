872b9ed033781ca5dc8f839cf859f52080ecd0fa
Nhan Le <nhanle2392@gmail.com>
2025-10-05 14:15:00 +0700

    sync with main repo

--- src/main/java/fr/xephi/authme/service/TeleportationService.java
+++ src/main/java/fr/xephi/authme/service/TeleportationService.java
@@ -29,7 +29,7 @@ import static fr.xephi.authme.settings.properties.RestrictionSettings.TELEPORT_U
  * Handles teleportation (placement of player to spawn).
  */
 public class TeleportationService implements Reloadable {
-    
+
     private final ConsoleLogger logger = ConsoleLoggerFactory.get(TeleportationService.class);
 
     @Inject
@@ -140,7 +140,7 @@ public class TeleportationService implements Reloadable {
             logger.debug("Teleporting `{0}` to spawn because of 'force-spawn after login'", player.getName());
             teleportToSpawn(player, true);
         } else if (settings.getProperty(TELEPORT_UNAUTHED_TO_SPAWN)) {
-            if (settings.getProperty(RestrictionSettings.SAVE_QUIT_LOCATION) && auth.getQuitLocY() != 0) {
+            if (settings.getProperty(RestrictionSettings.SAVE_QUIT_LOCATION)) {
                 Location location = buildLocationFromAuth(player, auth);
                 logger.debug("Teleporting `{0}` after login, based on the player auth", player.getName());
                 teleportBackFromSpawn(player, location);
